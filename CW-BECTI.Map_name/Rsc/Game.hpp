407f31b27bc9b67108334b8f177389c2b995d57e
dukee02 <68999242+dukee02@users.noreply.github.com>
2025-07-21 13:54:49 +0200

    Changed: default - Inf Marker now show icon for own units

--- CW-BECTI.Map_name/Rsc/Game.hpp
+++ CW-BECTI.Map_name/Rsc/Game.hpp
@@ -8,7 +8,7 @@ onLoadName = "VIO-BECTI Cold-War V2.0.6beta";
 onLoadMissionTime = false;
 overviewText = "VIO-BECTI Cold-War V2.0.6beta";
 overviewTextLocked = "VIO-BECTI Cold-War V2.0.6beta";
-onLoadMission = "VIO-BECTI Cold-War V2.0.6beta_250718";
+onLoadMission = "VIO-BECTI Cold-War V2.0.6beta_250720";
 briefingName = "VIO-BECTI Cold-War V2.0.6beta";
 
 //--- ArmA 3 Specifics
